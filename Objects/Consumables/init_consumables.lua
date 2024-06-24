44210b472f223758fa532d77e9c0bcdda8f0e15d
ItsFlowwey <73145114+GauntletGames-2086@users.noreply.github.com>
2024-06-23 19:20:52 -0800

    Fixing inconsistent file loading

--- Objects/Consumables/init_consumables.lua
+++ Objects/Consumables/init_consumables.lua
@@ -1,14 +1,14 @@
 local init_consumables = function(base_file_path)
-	local consumable_metafiles = NFS.getDirectoryItems(base_file_path.."\\consumables LuaList")
+	local consumable_metafiles = NFS.getDirectoryItems(base_file_path.."/consumables LuaList")
 	local consumables_to_inject = {}
-	local order_list = NFS.load(base_file_path.."\\consumables_config.lua")()
+	local order_list = NFS.load(base_file_path.."/consumables_config.lua")()
 
 	--assemble list of consumables_to_inject
 	for _, metafile in ipairs(consumable_metafiles) do
-		local consumable_files = NFS.getDirectoryItems(base_file_path.."\\consumables LuaList\\"..metafile)
+		local consumable_files = NFS.getDirectoryItems(base_file_path.."/consumables LuaList/"..metafile)
 		for _, file in ipairs(consumable_files) do
 			if string.find(file, ".lua") then
-				local consumable = NFS.load(base_file_path.."\\consumables LuaList\\"..metafile.."\\"..file)()
+				local consumable = NFS.load(base_file_path.."/consumables LuaList/"..metafile.."/"..file)()
 				consumable["order"] = (order_list[consumable.key] and order_list[consumable.key].order) or #order_list
 				if not order_list[consumable.key] then sendErrorMessage("MISSING FROM ORDER LIST: "..consumable.key) end
 				consumables_to_inject[#consumables_to_inject+1] = consumable
--- Objects/D6 Jokers/init_d6_jokers.lua
+++ Objects/D6 Jokers/init_d6_jokers.lua
@@ -1,12 +1,12 @@
 local init_d6_jokers = function(base_file_path)
-	local d6_joker_files = NFS.getDirectoryItems(base_file_path.."\\d6_joker LuaList")
+	local d6_joker_files = NFS.getDirectoryItems(base_file_path.."/d6_joker LuaList")
 	local d6_jokers_to_inject = {}
-	local order_list = NFS.load(base_file_path.."\\d6_jokers_config.lua")()
+	local order_list = NFS.load(base_file_path.."/d6_jokers_config.lua")()
 
 	--assemble list of d6_jokers_to_inject
 	for k, file in pairs(d6_joker_files) do
 		if string.find(file, ".lua") then
-			local d6_joker = NFS.load(base_file_path.."\\d6_joker LuaList\\"..file)()
+			local d6_joker = NFS.load(base_file_path.."/d6_joker LuaList/"..file)()
 			if not d6_joker.config["extra"] then d6_joker.config.extra = {} end
 			d6_joker.config.extra["local_d6_sides"] = {}
 			d6_joker["order"] = (order_list[d6_joker.key] and order_list[d6_joker.key].order) or #order_list
